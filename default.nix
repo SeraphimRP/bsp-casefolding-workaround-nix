59de19a96d1860e9699aed2921b02a86da41934a
Seraphim R. Pardee <me@srp.life>
2025-04-05 01:12:33 -0400

    Format it all.

--- default.nix
+++ default.nix
@@ -1,39 +1,46 @@
 {
-    config,
-    lib,
-    dream2nix,
-    ...
-}: let pyproject = lib.importTOML (config.mkDerivation.src + /pyproject.toml); in {
-    imports = [ dream2nix.modules.dream2nix.pip ];
+  config,
+  lib,
+  dream2nix,
+  ...
+}:
+let
+  pyproject = lib.importTOML (config.mkDerivation.src + /pyproject.toml);
+in
+{
+  imports = [ dream2nix.modules.dream2nix.pip ];
 
-    deps = { nixpkgs, ... }: {
-        python = nixpkgs.python313;
+  deps =
+    { nixpkgs, ... }:
+    {
+      python = nixpkgs.python313;
     };
 
-    inherit (pyproject.project) name version;
+  inherit (pyproject.project) name version;
 
-    mkDerivation = {
-        src = lib.cleanSourceWith {
-            src = lib.cleanSource ./.;
-            filter = name: type:
-                !(builtins.any (x: x) [
-                    (lib.hasSuffix ".nix" name)
-                    (lib.hasPrefix "." (builtins.baseNameOf name))
-                    (lib.hasSuffix "flake.lock" name)
-                ]);
-        };
+  mkDerivation = {
+    src = lib.cleanSourceWith {
+      src = lib.cleanSource ./.;
+      filter =
+        name: type:
+        !(builtins.any (x: x) [
+          (lib.hasSuffix ".nix" name)
+          (lib.hasPrefix "." (builtins.baseNameOf name))
+          (lib.hasSuffix "flake.lock" name)
+        ]);
     };
+  };
 
-    buildPythonPackage = {
-        pyproject = true;
-        pythonImportsCheck = [
-            "bsp_casefolding_workaround"
-        ];
-    };
+  buildPythonPackage = {
+    pyproject = true;
+    pythonImportsCheck = [
+      "bsp_casefolding_workaround"
+    ];
+  };
 
-    paths.lockFile = "lock.${config.deps.stdenv.system}.json";
-    pip = {
-        requirementsList = pyproject.build-system.requires or [] ++ pyproject.project.dependencies;
-        flattenDependencies = true;
-    };
+  paths.lockFile = "lock.${config.deps.stdenv.system}.json";
+  pip = {
+    requirementsList = pyproject.build-system.requires or [ ] ++ pyproject.project.dependencies;
+    flattenDependencies = true;
+  };
 }
--- flake.nix
+++ flake.nix
@@ -1,55 +1,75 @@
 {
-    description = "A NixOS background service for extracting BSP data in Source games.";
+  description = "A NixOS background service for extracting BSP data in Source games.";
 
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-        dream2nix.url = "github:nix-community/dream2nix";
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    dream2nix.url = "github:nix-community/dream2nix";
+  };
 
-    outputs = { self, nixpkgs, dream2nix }: let nixosModule = { config, lib, pkgs, ... }: {
-        options.services.bsp-casefolding-workaround = {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      dream2nix,
+    }:
+    let
+      nixosModule =
+        {
+          config,
+          lib,
+          pkgs,
+          ...
+        }:
+        {
+          options.services.bsp-casefolding-workaround = {
             enable = lib.mkEnableOption "BSP casefolding workaround service";
 
             watch_dirs = lib.mkOption {
-                type = lib.types.listOf lib.types.str;
-                default = null;
-                example = lib.options.literalExpression ''[
-                  "/home/user/.steam/steamapps/common/Counter-Strike Source/cstrike/download"
-                ];'';
-                description = "A list of download directories to watch for BSPs.";
+              type = lib.types.listOf lib.types.str;
+              default = null;
+              example = lib.options.literalExpression ''
+                [
+                                  "/home/user/.steam/steamapps/common/Counter-Strike Source/cstrike/download"
+                                ];'';
+              description = "A list of download directories to watch for BSPs.";
             };
-        };
+          };
 
-        config = lib.mkIf config.services.bsp-casefolding-workaround.enable {
+          config = lib.mkIf config.services.bsp-casefolding-workaround.enable {
             systemd.user.services.bsp-casefolding-workaround = {
-                Unit = {
-                    Description = "BSP casefolding workaround service";
-                };
-                Install = {
-                    WantedBy = [ "default.target" ];
-                };
-                Service = {
-                    ExecStart = "${self.packages.${pkgs.system}.default}/bin/bsp-casefolding-workaround ${lib.escapeShellArgs config.services.bsp-casefolding-workaround.watch_dirs}";
-                    Restart = "always";
-                    Type = "simple";
-                };
+              Unit = {
+                Description = "BSP casefolding workaround service";
+              };
+              Install = {
+                WantedBy = [ "default.target" ];
+              };
+              Service = {
+                ExecStart = "${
+                  self.packages.${pkgs.system}.default
+                }/bin/bsp-casefolding-workaround ${lib.escapeShellArgs config.services.bsp-casefolding-workaround.watch_dirs}";
+                Restart = "always";
+                Type = "simple";
+              };
             };
+          };
         };
-    }; eachSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" ]; in {
-        packages = eachSystem (system: {
-            default = dream2nix.lib.evalModules {
-                packageSets.nixpkgs = nixpkgs.legacyPackages.${system};
-                modules = [
-                    ./default.nix
-                    {
-                        paths.projectRoot = ./.;
-                        paths.projectRootFile = "flake.nix";
-                        paths.package = ./.;
-                    }
-                ];
-            };
-        });
+      eachSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
+    in
+    {
+      packages = eachSystem (system: {
+        default = dream2nix.lib.evalModules {
+          packageSets.nixpkgs = nixpkgs.legacyPackages.${system};
+          modules = [
+            ./default.nix
+            {
+              paths.projectRoot = ./.;
+              paths.projectRootFile = "flake.nix";
+              paths.package = ./.;
+            }
+          ];
+        };
+      });
 
-        nixosModules.default = nixosModule;
+      nixosModules.default = nixosModule;
     };
 }
